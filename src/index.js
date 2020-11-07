34b0459b10e3ab4ebe0973d3fbeaf18942ea13c2
Bilal Arif <36725442+BilalArif-34@users.noreply.github.com>
2020-11-07 22:19:18 +0300

    deleted app import

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
 import Room from './Room';
 
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
- 
+
     <Room />
   </React.StrictMode>,
   document.getElementById('root')
